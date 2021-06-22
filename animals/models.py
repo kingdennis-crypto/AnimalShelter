e0a28142db55e86ad5cfbcc3491a61507ce9f86b
kingdennis-crypto <dennismoes@me.com>
2021-06-22 09:26:04 +0200

    Added the animals model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
+import uuid
 
-# Create your models here.
+class animal(models.Model):
+  id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  name = models.CharField(max_length=64)
+  age = models.IntegerField()
+  image = models.ImageField(upload_to='images/%Y/%m/%d/')
+  
+  def __str__(self):
+    return self.name
